971f55f16eb9e4ed690f7d6e35b06f7aab165a10
MaciejBabicki <macbabicki@gmail.com>
2024-06-03 12:40:00 +0200

    deployment

--- src/main/java/pl/programmers/programmer/controller/GithubController.java
+++ src/main/java/pl/programmers/programmer/controller/GithubController.java
@@ -1,24 +1,57 @@
 package pl.programmers.programmer.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.programmers.programmer.entity.GithubRepository;
+import pl.programmers.programmer.pojo.GithubRepositoryDto;
+import pl.programmers.programmer.repository.GithubRepositoryRepo;
+import pl.programmers.programmer.service.GithubRepositoryService;
 import pl.programmers.programmer.service.importservice.GithubImportService;
+import pl.programmers.programmer.service.importservice.TechnologiesInRepo;
 
 import java.util.List;
 
 @RestController
 @CrossOrigin("*")
 @RequiredArgsConstructor
-@RequestMapping("/githubrepositories")
+@RequestMapping("/repos")
 public class GithubController {
     private final GithubImportService githubImportService;
+    private final GithubRepositoryService githubRepositoryService;
+    private final TechnologiesInRepo technologiesInRepo;
+    private final GithubRepositoryRepo githubRepositoryRepo;
+
+    @PostMapping
+    public GithubRepositoryDto createGithubRepository() {
+        GithubRepository githubRepositoryRequest = new GithubRepository();
+        return githubRepositoryService.createRepository(githubRepositoryRequest);
+    }
+
+    @PostMapping("import-save")
+    public GithubRepositoryDto saveGithubRepository() {
+        String name = "programmers";
+        GithubRepository githubRepository = githubImportService.getGithubRepository(name);
+        return githubRepositoryService.createRepository(githubRepository);
+    }
 
     @GetMapping
-    public List<GithubRepository> getGithub() {
+    public List<GithubRepository> getGithubRepositories() {
         return githubImportService.getGithubRepositories();
+    }
+
+    @GetMapping("dbrepositories")
+    public List<GithubRepositoryDto> githubRepositoriesFromDB() {
+        return githubRepositoryService.getGithubRepositories();
+    }
+
+    @GetMapping("one")
+    public GithubRepository getOneRepository() {
+        String aa = "programmers";
+        return githubImportService.getGithubRepository(aa);
+    }
+
+    @GetMapping("/technologies")
+    public void getTechnologies() {
+        technologiesInRepo.showTechnologies();
     }
 }
