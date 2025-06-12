d98f73fa5cd39b00f9adabd67290edbb655189de
MaciejBabicki <macbabicki@gmail.com>
2025-06-12 21:13:16 +0200

    fix(GithubImportService) : remove useless methods

--- src/main/java/pl/programmers/programmer/service/importservice/GithubImportService.java
+++ src/main/java/pl/programmers/programmer/service/importservice/GithubImportService.java
@@ -1,12 +1,13 @@
 package pl.programmers.programmer.service.importservice;
 
-import pl.programmers.programmer.entity.Branch;
-import pl.programmers.programmer.entity.GithubRepository;
-import pl.programmers.programmer.exception.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import pl.programmers.programmer.entity.Branch;
+import pl.programmers.programmer.entity.GithubRepository;
+import pl.programmers.programmer.exception.ResourceNotFoundException;
+import pl.programmers.programmer.pojo.GithubRepositoryResponse;
 
 import java.util.Arrays;
 import java.util.List;
@@ -16,11 +17,30 @@ public class GithubImportService {
     private final RestTemplate restTemplate;
     private final String githubApiUrl;
     private final String login;
+    private final String githubSearchQuery;
+    private final String language;
+    private final String githubApiUrl2;
 
-    public GithubImportService(RestTemplate restTemplate, @Value("${github.api.url}")String githubApiUrl, @Value("${github.login}")String login) {
+    public GithubImportService(RestTemplate restTemplate, @Value("${github.api.url}") String githubApiUrl,
+                               @Value("${github.login}") String login, @Value("${github.search.query}") String githubSearchQuery,
+                               @Value("${github.search.language}") String language,
+                               @Value("${github.api.url2}") String githubApiUrl2) {
         this.restTemplate = restTemplate;
         this.githubApiUrl = githubApiUrl;
         this.login = login;
+        this.githubSearchQuery = githubSearchQuery;
+        this.language = language;
+        this.githubApiUrl2 = githubApiUrl2;
+    }
+
+    public List<GithubRepository> searchRepositoriesWithTitleAndLanguage() {
+        String url = githubApiUrl2 + "?q=" + githubSearchQuery + "+language:" + language;
+        GithubRepositoryResponse responseEntity = restTemplate.
+                getForObject(url, GithubRepositoryResponse.class);
+        if (responseEntity != null) {
+            return responseEntity.getItems();
+        }
+        return List.of();
     }
 
     public List<GithubRepository> getGithubRepositories() {
@@ -34,20 +54,10 @@ public class GithubImportService {
                 .peek(this::fetchBranches)
                 .toList();
     }
-    public GithubRepository getGithubRepository(String repositoryName) {
-        String url = githubApiUrl + "/repos/" + login + "/" + repositoryName;
-        ResponseEntity<GithubRepository> responseEntity = restTemplate.getForEntity(url, GithubRepository.class);
-        if (responseEntity.getBody() == null) {
-            throw new ResourceNotFoundException();
-        }
-        GithubRepository repository = responseEntity.getBody();
-        fetchBranches(repository);
-        return repository;
-    }
 
-    void fetchBranches(GithubRepository githubRepository) {
+    List<Branch> fetchBranches(GithubRepository githubRepository) {
         String branchesUrl = githubRepository.getUrl() + "/branches";
         ResponseEntity<Branch[]> branchesResponseEntity = restTemplate.getForEntity(branchesUrl, Branch[].class);
-        Branch[] branches = branchesResponseEntity.getBody();
+        return Arrays.asList(branchesResponseEntity.getBody());
     }
 }
